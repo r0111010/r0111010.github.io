3c44330d1cad6dd14cfa78e5ec6e8328c92ee31e
metis <r01110010m@gmail.com>
2021-08-20 00:26:34 +0430

    move to docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  generate:{
+    dir:"docs" 
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - telegram',
